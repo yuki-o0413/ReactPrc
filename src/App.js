60b4176b709feb436ebf620f05700e44077efdb1
yoji <ojimay@colabmix.co.jp>
2021-01-23 14:38:38 +0900

    added tab

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import { useState } from 'react'; // 追加
+import { useState } from 'react';
 import { List } from "./List";
+import { Form } from "./Form";
 
 function App() {
   const [description, setDescription] = useState('クリック前の表示');
+  const [tab, setTab] = useState('list');
+
+  const changeDescription = () => {
+    setDescription('クリック後の表示です。')
+  }
 
   return (
     <div>
+      <header>
+        <ul>
+          <li onClick={() => setTab('list')}>リスト</li>
+          <li onClick={() => setTab('form')}>フォーム</li>
+        </ul>
+      </header>
+      <hr />
       {description}
-      <List title="取扱言語一覧" />
-      <button onClick={() => setDescription}>ボタン</button>
+      {
+        tab === 'list' ? <List title="取扱言語一覧" /> : <Form />
+      }
+      <button onClick={changeDescription}>ボタン</button>
     </div>
   );
 }
