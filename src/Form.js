a6caf87ce03196214ce6316161d2aed820431c03
yoji <ojimay@colabmix.co.jp>
2021-01-23 22:26:55 +0900

    入門9: Portalを利用したモーダルの作成

--- src/Form.js
+++ src/Form.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import styled from 'styled-components';
 import { Button } from "./components/button";
 import { TabBodyContainer } from "./components/tab-body-container";
+import { FormModal } from "./FormModal";
 
 const Label = styled.label`
   display: flex;
@@ -23,10 +24,11 @@ const FormButton = styled(Button)`
 
 export const Form = ({onAddLang}) => {
   const [text, setText] = useState('');
+  const [showModal, setShowModal] = useState(false);
 
   const submitForm = (e) => {
     e.preventDefault();
-    onAddLang(text);
+    setShowModal(true);
   }
   return (
     <TabBodyContainer titled="新しい言語の追加">
@@ -39,6 +41,13 @@ export const Form = ({onAddLang}) => {
           <FormButton>追加</FormButton>
         </ButtonContainer>
       </form>
+      {
+        showModal &&
+          <FormModal
+            confirm={()=> onAddLang(text)}
+            cancel={()=> setShowModal(false)}
+          />
+      }
     </TabBodyContainer>
   )
 }
