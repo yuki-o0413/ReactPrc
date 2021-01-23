f4e585d8cd28f26e7e43e389e4590ad08b005f5b
yoji <ojimay@colabmix.co.jp>
2021-01-23 17:46:04 +0900

    styled header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import { useState, useEffect  } from 'react';
+import { useState, useEffect  } from 'react';//useEffectのインポートを追加
+import styled from 'styled-components';
 import { List } from "./List";
 import { Form } from "./Form";
-import {getLanguages, LANGUAGES} from "./const/languages";
+import {getLanguages} from "./const/languages";
+
+const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  padding: 24px 64px 0;
+  border-bottom: 1px solid #E0E0E0;
+`
+
+const HeaderUl = styled.ul`
+  display: flex;
+  margin: 0;
+  padding: 0;
+`
+const HeaderLi = styled.li`
+  list-style: none;
+  padding: 4px 12px;
+  cursor: pointer;
+  border-bottom: ${props => props.focused ? '2px solid #F44336' : 'none' };
+`
 
 function App() {
   const [tab, setTab] = useState('list');
@@ -11,7 +31,7 @@ function App() {
   useEffect(() => {
     console.log('App.js:useEffect');
     fetchLanguages();
-  }, []);
+  }, [tab]);
 
   const fetchLanguages = async () =>{
     const languages = await getLanguages();
@@ -26,12 +46,12 @@ function App() {
 
   return (
     <div>
-      <header>
-        <ul>
-          <li onClick={() => setTab('list')}>リスト</li>
-          <li onClick={() => setTab('form')}>フォーム</li>
-        </ul>
-      </header>
+      <Header>
+        <HeaderUl>
+          <HeaderLi onClick={() => setTab('list')}>リスト</HeaderLi>
+          <HeaderLi onClick={() => setTab('form')}>フォーム</HeaderLi>
+        </HeaderUl>
+      </Header>
       <hr />
       {
         tab === 'list' ? <List langs={langs}/> : <Form onAddLang={addLang}/>
